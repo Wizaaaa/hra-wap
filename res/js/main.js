c3423a8fd5a4b93d181d582ea1cb636819fce547
skola-pc <jakubsalaky@gmail.com>
2025-03-06 09:24:43 +0100

    more key binds

--- res/js/main.js
+++ res/js/main.js
@@ -5,7 +5,9 @@ class Player {
         this.w = w;
         this.h = h;
         this.c = c;
+        this.bV = 2 // basic velocity
         this.v = 3; //velocity
+        this.cWH = 5; //change Width Height
     }
 
     draw(ctx) {
@@ -14,10 +16,42 @@ class Player {
     }
 
     update(keys) {
-        if (keys["w"]) this.y -= this.v;
-        if (keys["s"]) this.y += this.v;
-        if (keys["a"]) this.x -= this.v;
-        if (keys["d"]) this.x += this.v;
+        if (keys["w"]) {
+            if (this.y - this.v > 0) {
+                this.y -= this.v;
+            }
+        }
+        if (keys["s"]) {
+            if (this.y + this.h + this.v < gameCanvas.height) {
+                this.y += this.v;
+            }
+        }
+        if (keys["a"]) {
+            if (this.x - this.v > 0) {
+                this.x -= this.v;
+            }
+        }
+        if (keys["d"]) {
+            if (checkCollision) {
+                if (this.x + this.w + this.v < gameCanvas.width) {
+                    this.x += this.v;
+                }
+            }
+        }
+        if (keys[" "]) {
+            this.v = 10;
+        } else {
+            this.v = this.bV;
+        }
+        if (keys["e"] && (this.w + this.cWH <= 200 || this.h + this.cWH <= 200)) {
+            this.w += this.cWH;
+            this.h += this.cWH;
+        }
+        if (keys["r"] && (this.w - this.cWH > 0 || this.h - this.cWH > 0)) {
+            this.w -= this.cWH;
+            this.h -= this.cWH;
+    
+        }
     }
 }
 
@@ -45,11 +79,28 @@ const gameLoop = () => {
 
     //update
     myPlayer.update(keys);
+    checkCollision(myPlayer, testObject);
 
     //draw
     myPlayer.draw(ctx);
-
+    ctx.fillStyle = testObject.c;
+    ctx.fillRect(testObject.x, testObject.y, testObject.w, testObject.h);
     requestAnimationFrame(gameLoop);
 }
 
 requestAnimationFrame(gameLoop);
+
+let testObject = {
+    x: 100,
+    y: 200,
+    w: 100,
+    h: 100,
+    c: "blue"
+}
+
+
+const checkCollision = (object1, object2) => {
+    if (object1.x + object1.w > object2.x) {
+        return true;
+    }
+}
